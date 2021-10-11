3fd7114d2c4012bf36cfd4a260063d95e4fad4aa
John Sangalang <sangalangjl@gmail.com>
2021-10-11 15:49:24 -0400

    commit tree 7a3585adbb26a5244c638b9b0b821e13a3c03ba2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+      Student.by_name(params[:name])
+    else
+      Student.all
+    end
     render json: students
+  end
+
+  def show
+    render json: Student.find_by(id: params[:id])
   end
 
 end
